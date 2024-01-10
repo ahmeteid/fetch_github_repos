f58da112843c4900c482df6139051d18826a0dfb
ahmeteid <ahmeteid355@gmail.com>
2024-01-11 01:17:30 +0300

    Loop On Data and Create Elements

--- main.js
+++ main.js
@@ -21,10 +21,46 @@ function getRepos() {
 
         fetch('https://api.github.com/users/ahmeteid/repos')
         .then(response => response.json())
-        .then(linkReposData => console.log(linkReposData))
+        .then(repositories => {
+            // Empty the Container
+        reposData.innerHTML = "";
+
+        // Loop On Repositories
+        repositories.forEach(repo => {
+            
+            // create Main Div Element
+            let mainDiv = document.createElement("div");
+
+            // Create Repo Repo text
+            let repoName = document.createTextNode(repo.name);
+
+            // Append the text to main Div
+            mainDiv.appendChild(repoName);
+
+            // Create Repo Url
+            let theUrl = document.createElement("a");
+
+            // Create Repo Url text
+            let theUrlText = document.createTextNode("visit");
+
+            // Append the Repo Url text to Anchor tag
+            theUrl.appendChild(theUrlText);
+
+            // Add the hypertext refrence ("href")
+            theUrl.href = `https://github.com/users/ahmeteid/${repo.name}`;
+
+            // Set attreibute blank
+            theUrl.setAttribute("target", "_blank");
+
+            // Append Url anchor to Main Div
+            mainDiv.appendChild(theUrl);
+
+            // Append the Main Div to continer
+            reposData.appendChild(mainDiv);
+        })
         // .catch(error => console.error('Error:', error));
 
-        // Empty the Container
-        reposData.innerHTML = "";
+        
+        })
     }
 }
